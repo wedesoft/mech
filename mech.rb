9ba040dee3ca3d7f8a2e23a3e252d40d83e956bc
Jan Wedekind (Dr) <jan@wedesoft.de>
2017-03-26 22:37:13 +0100

    Update sprite position using game controller

--- mech.rb
+++ mech.rb
@@ -1,31 +1,90 @@
 #!/usr/bin/env ruby
+require 'matrix'
 require 'sdl2'
 
 
-SDL2.init SDL2::INIT_VIDEO | SDL2::INIT_JOYSTICK
+class Sprite
+  def initialize renderer, file_name
+    @sprite = renderer.create_texture_from SDL2::Surface.load file_name
+  end
 
-window = SDL2::Window.create 'Mech', 0, 0, 640, 480, SDL2::Window::Flags::FULLSCREEN_DESKTOP
-renderer = window.create_renderer -1, 0
-joystick = nil
-red   = 0xA0
-green = 0xA0
-blue  = 0xA0
-
-loop do
-  while event = SDL2::Event.poll
-    case event
-    when SDL2::Event::Quit
-      exit
-    when SDL2::Event::JoyDeviceAdded
-      joystick = SDL2::Joystick.open event.which
-    when SDL2::Event::JoyAxisMotion
-      red   = (event.value + 32768) * 255 / 65536 if event.axis == 0
-      green = (event.value + 32768) * 255 / 65536 if event.axis == 1
-      blue  = (event.value + 32768) * 255 / 65536 if event.axis == 3
-    end
-
-    renderer.draw_color = [red, green, blue]
-    renderer.clear
-    renderer.present
+  def paint renderer, position
+    x, y = *position
+    w, h = @sprite.w, @sprite.h
+    renderer.copy @sprite, nil, SDL2::Rect[x - w / 2, y - h / 2, w, h]
   end
 end
+
+class Physics
+  attr_reader :position
+  attr_reader :speed
+
+  def initialize
+    @position = Vector[320, 240]
+    @speed = Vector[0, 0]
+  end
+
+  def speed_x= value
+    @speed = Vector[value, @speed[1]]
+  end
+
+  def speed_y= value
+    @speed = Vector[@speed[0], value]
+  end
+
+  def update dt
+    @position += Vector[*@speed] * dt
+  end
+end
+
+
+class MechGame
+  def initialize
+    SDL2.init SDL2::INIT_VIDEO | SDL2::INIT_JOYSTICK
+    @window = SDL2::Window.create 'Mech', 0, 0, 640, 480, 0 # SDL2::Window::Flags::FULLSCREEN_DESKTOP
+    @renderer = @window.create_renderer -1, 0
+    @mech_sprite = Sprite.new @renderer, 'mech.png'
+    @mech_physics = Physics.new
+    @joystick = nil
+    @quit = false
+  end
+
+  def handle_events
+    while event = SDL2::Event.poll
+      case event
+      when SDL2::Event::Quit
+        @quit = true
+      when SDL2::Event::JoyDeviceAdded
+        @joystick = SDL2::Joystick.open event.which
+      when SDL2::Event::JoyAxisMotion
+        @mech_physics.speed_x = event.value * 100.0 / 32768 if event.axis == 0
+        @mech_physics.speed_y = event.value * 100.0 / 32768 if event.axis == 1
+      end
+    end
+  end
+
+  def paint
+    @renderer.draw_color = [0xA0, 0xA0, 0xA0]
+    @renderer.clear
+    @mech_sprite.paint @renderer, @mech_physics.position
+    @renderer.present
+  end
+
+  def run
+    time = Time.new.to_f
+    while not @quit
+      paint
+      handle_events
+      dt = Time.new.to_f - time
+      if dt < 0.04
+        sleep 0.04 - dt
+        dt = 0.04
+      end
+      @mech_physics.update dt
+      time += dt
+    end
+  end
+end
+
+
+MechGame.new.run
